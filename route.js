31842c7d626fcad2592e44c45250bc2ce8fd11ef
AmanKumar2397 <ak1315257@gmail.com>
2026-01-31 16:25:09 +0530

    Add files via upload

---
+++ route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Products routes
+router.get('/products', (req, res) => {
+  res.send('Here is the list of all products.');
+});
+
+router.post('/products', (req, res) => {
+  res.send('A new product has been added.');
+});
+
+// Categories routes
+router.get('/categories', (req, res) => {
+  res.send('Here is the list of all categories.');
+});
+
+router.post('/categories', (req, res) => {
+  res.send('A new category has been created.');
+});
+
+module.exports = router;
